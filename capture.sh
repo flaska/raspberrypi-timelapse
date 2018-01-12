b5639708805b97c4c6ffe1a1b988d875ed0882e7
Jakub <jakub.flaska@gmail.com>
2018-01-11 22:45:53 -0500

    done

--- capture.sh
+++ capture.sh
@@ -3,5 +3,6 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H%M")
 W=1476
 H=972
 O=/home/pi/camera/recorded/timelapse_$TIMESTAMP.jpg
-R=90
-raspistill -o $O -w $W -h $H -rot $R
+ROTATION=90
+DELAY="-t 1"
+raspistill -o $O -w $W -h $H -rot $ROTATION
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require("express")
-, index = require("serve-index");
+, index = require("serve-index")	
+, readdirLife = require('readdir-life');
+;
 
 var app = express();
 
 app.use(express.static('recorded'));
-app.use("/", index('recorded'));
+app.use("/camera/recorded", index('recorded'));
+
+app.get("/camera", function(req, res){
+	readdirLife.latest("./recorded")
+    		.then(function(lat){
+			res.send(lat.file);
+	});	
+});
 
 app.listen(3000, function(){
 	console.log("WebServer listening on port :3000");
